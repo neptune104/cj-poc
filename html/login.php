1370eea46e085b0be93c41eaf9343fa94016ccd2
neptune104 <35022983+neptune104@users.noreply.github.com>
2019-09-25 20:14:24 +0900

    Update login.php

--- html/login.php
+++ html/login.php
@@ -23,7 +23,7 @@ if($user_id !='' && $pwd !='' )
 ?><!DOCTYPE html>
 <html>
 <head>
-<title>G-LINK</title>
+<title>G-LINK1</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="/glink_login/style.css" rel="stylesheet" media="screen">
